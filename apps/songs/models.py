737d354cced89ab54879fc92f0d3bbd6fad1debc
hengage <tunecarterkillz@gmail.com>
2021-09-09 03:17:37 +0100

    created initial models for 'song' and 'album' and set up 'AWS S3' conf.

--- apps/songs/models.py
+++ apps/songs/models.py
@@ -2,3 +2,42 @@
 This model contains all the database design models for 'sonngs, 'albums' and 'playlists'.
 """
 
+from django.db import models
+from django.conf import settings
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+class Album(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+         on_delete=models.CASCADE,
+         related_name='artist'
+    )
+    album_file = models.FileField()
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.PROTECT,
+        related_name='genre'
+    )
+    year = models.IntegerField(max_length=4)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    duration = models.DecimalField(blank=True)
+    cover_image = models.FileField()
+    
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.ForeignKey('CustomUser', 
+    on_delete=models.CASCADE, 
+    related_name='artist'
+    )
+    song_file = models.FileField()
+    genre = models.ForeignKey(Genre)
+    year = models.IntegerField(max_length=4)
+    duration = models.DecimalField(blank=True)
+    cover_image = models.FileField()
+
+    
