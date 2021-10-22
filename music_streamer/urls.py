cff6c608b12d6edb584f59a91d4ce964b8a46b41
Ogbuchi Arinze <ogbuchiarinze01@gmail.com>
2021-10-22 01:05:19 +0100

    I added the authentication with django all auth. I encountered problems while trying to create a superuser to test the feature. Social login can be easily implemented , the socia keys just needs to be gotten from the providers (google).

--- music_streamer/urls.py
+++ music_streamer/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('songs.urls')),
+    path("admin/", admin.site.urls),
+    path("api/", include("songs.urls")),
+    path("accounts/", include("allauth.urls")),
 ]
